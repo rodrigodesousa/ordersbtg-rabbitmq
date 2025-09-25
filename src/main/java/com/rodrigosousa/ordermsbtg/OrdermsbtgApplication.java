b9d78ab99d35290f650aa0afbf0af53bbff18a5a
Rodrigo <rodrigudesousa@gmail.com>
2025-09-25 14:15:32 -0400

    app created

---
+++ src/main/java/com/rodrigosousa/ordermsbtg/OrdermsbtgApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigosousa.ordermsbtg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdermsbtgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdermsbtgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodrigosousa/ordermsbtg/OrdermsbtgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigosousa.ordermsbtg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdermsbtgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
