9fbfaa75addee9a7eddaa09b6575bed8557f885d
Rodrigo <rodrigudesousa@gmail.com>
2025-09-25 14:32:06 -0400

    feat: config mongodb and rabbitmq

---
+++ src/main/java/com/rodrigosousa/ordermsbtg/config/RabbitMqConfig.java
@@ -0,0 +1,9 @@
+package com.rodrigosousa.ordermsbtg.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfig {
+
+    public static final String ORDER_CREATED_QUEUE = "btg-pactual-order-created";
+}
